740d4c22728e4bb621b200fb3f6a130511ff24c6
Alex7ey <den.sab.di@yandex.ru>
2024-11-06 13:37:42 +0300

    Squashed commit of the following:
    commit b4082b9108ed45d9606e51f28cdae66591a7ddd0
    Author: Alex7ey <den.sab.di@yandex.ru>
    Date:   Wed Nov 6 13:32:02 2024 +0300
    
        Squashed commit of the following:
    
        commit 9d2c1614b2f14e65133587372c47ceba33a94e5e
        Author: Alex7ey <den.sab.di@yandex.ru>
        Date:   Wed Nov 6 03:19:41 2024 +0300
    
            Ready StateMachine (Raw)
    
        commit 1ae48504befe2cd848d02879da00dc684a9abedb
        Author: Alex7ey <den.sab.di@yandex.ru>
        Date:   Wed Nov 6 00:06:21 2024 +0300
    
            FixUpdate
    
        commit 38e8b02789e172ef277743eebc774847353a44aa
        Author: Alex7ey <den.sab.di@yandex.ru>
        Date:   Sat Nov 2 22:19:39 2024 +0300
    
            Add State and Tansitions
    
        commit 6a0d53d828e94c3847116408033ed1cb98ac03fe
        Author: Alex7ey <den.sab.di@yandex.ru>
        Date:   Thu Oct 31 15:50:19 2024 +0300
    
            Reapply "Merge branch 'main' into Alex"
    
            This reverts commit 020a642dbd506a34a84dafefaa6c0094c02d8a46.
    
        commit 020a642dbd506a34a84dafefaa6c0094c02d8a46
        Author: Alex7ey <den.sab.di@yandex.ru>
        Date:   Thu Oct 31 15:49:34 2024 +0300
    
            Revert "Merge branch 'main' into Alex"
    
            This reverts commit e5c5fcd18165145ce9eda63533eb3ecb90888bd4, reversing
            changes made to 908a465aa2473864b4020e3b32260c2010793dcd.
    
        commit e5c5fcd18165145ce9eda63533eb3ecb90888bd4
        Merge: 908a465 f648255
        Author: Alex7ey <den.sab.di@yandex.ru>
        Date:   Thu Oct 31 15:49:04 2024 +0300
    
            Merge branch 'main' into Alex
    
        commit 908a465aa2473864b4020e3b32260c2010793dcd
        Author: Alex7ey <den.sab.di@yandex.ru>
        Date:   Thu Oct 31 15:48:29 2024 +0300
    
            Add ScriptGeneratePath
    
        commit d9c27cbbb4645bf8c6a0051b3be84aded436478c
        Merge: ebd2499 f52d8fd
        Author: Alex7ey <den.sab.di@yandex.ru>
        Date:   Thu Oct 31 15:41:52 2024 +0300
    
            Merge branch 'main' into Alex
    
        commit ebd24996d6f0ad02d09c69bac9f9d0c487d2ba77
        Merge: 17f2371 e9af6b3
        Author: Alex7ey <den.sab.di@yandex.ru>
        Date:   Wed Oct 30 00:41:36 2024 +0300
    
            Merge branch 'main' into Alex
    
    commit 9525048c8ae81ef82b71cb19339e6ebdbd2d2c8b
    Author: kerosin122 <152972149+kerosin122@users.noreply.github.com>
    Date:   Sat Nov 2 14:21:23 2024 +0400
    
        buttons

---
+++ Assets/Scripts/Pause.cs
@@ -0,0 +1,60 @@
+using UnityEngine.UI;
+using UnityEngine;
+
+public class Pause : MonoBehaviour
+{
+   private Text buttonText;
+    public Button pauseButton;
+    private bool isPaused = false;
+
+    void Start()
+    {
+        // Убедитесь, что кнопка назначена
+        if (pauseButton != null)
+        {
+            buttonText = pauseButton.GetComponentInChildren<Text>();
+            if (buttonText == null)
+            {
+                Debug.LogError("нет привязанного текста к кнопке.");
+                return;
+            }
+
+            pauseButton.onClick.AddListener(TogglePause);
+            UpdateButtonText();
+        }
+        else
+        {
+            Debug.LogError("кнопка не привязанна.");
+        }
+    }
+
+    void TogglePause()
+    {
+        if (isPaused)
+        {
+            // Снимаем игру с паузы
+            Time.timeScale = 1f;
+            isPaused = false;
+        }
+        else
+        {
+            // Ставим игру на паузу
+            Time.timeScale = 0f;
+            isPaused = true;
+        }
+
+        UpdateButtonText();
+    }
+
+    void UpdateButtonText()
+    {
+        if (isPaused)
+        {
+            buttonText.text = "Продолжить";
+        }
+        else
+        {
+            buttonText.text = "Пауза";
+        }
+    }
+}
